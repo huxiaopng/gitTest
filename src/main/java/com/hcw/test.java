3456d8fa17a868cb98b623775e52e1466b40fccf
hcw <3022192504@qq.com>
2021-05-30 10:58:09 +0800

    hot-fix commit gitTest

--- src/main/java/com/hcw/test.java
+++ src/main/java/com/hcw/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello hcw");
         System.out.println("what do you du");
+        System.out.println("dududu");
     }
 }
